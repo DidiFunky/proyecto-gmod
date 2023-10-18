229b2a6b8c45da4b7f0fcb11c13b90f02a2c3fd8
DidiFunky <johandro007@gmail.com>
2023-10-18 11:36:13 -0500

    se hacen cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css';
+import NavbarRes from './components/containers/Navbar';
 import Navbar from './components/containers/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <NavbarRes/>
     </div>
   );
 }
